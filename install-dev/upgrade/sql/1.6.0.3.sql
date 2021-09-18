75714b243082cd4599490b854db31d39db308cf4
andrewua85 <36643871+andrewua85@users.noreply.github.com>
2021-09-19 01:15:35 +0300

    Update 1.6.0.3.sql
    1: HTTP 200 - parsererror -
    Deprecated: array_key_exists(): Using array_key_exists() on objects is deprecated. Use isset() or property_exists() instead in C:\OpenServer\domains\prestashop\src\Adapter\EntityMapper.php on line 99
    
    Deprecated: array_key_exists(): Using array_key_exists() on objects is deprecated. Use isset() or property_exists() instead in C:\OpenServer\domains\prestashop\src\Adapter\EntityMapper.php on line 99
    
    Deprecated: array_key_exists(): Using array_key_exists() on objects is deprecated. Use isset() or property_exists() instead in C:\OpenServer\domains\prestashop\src\Adapter\EntityMapper.php on line 99
    
    Deprecated: array_key_exists(): Using array_key_exists() on objects is deprecated. Use isset() or property_exists() instead in C:\OpenServer\domains\prestashop\src\Adapter\EntityMapper.php on line 99
    {"success":true,"message":""}

--- install-dev/upgrade/sql/1.6.0.3.sql
+++ install-dev/upgrade/sql/1.6.0.3.sql
@@ -4,7 +4,7 @@ ALTER TABLE `PREFIX_theme` ADD `default_left_column` TINYINT( 1 ) NOT NULL DEFAU
 ALTER TABLE `PREFIX_theme` ADD `default_right_column` TINYINT( 1 ) NOT NULL DEFAULT '1';
 ALTER TABLE `PREFIX_theme` ADD `product_per_page` INT UNSIGNED NOT NULL;
 
-ALTER TABLE `PREFIX_tab_lang` CHANGE `name` `name` VARCHAR(64) DEFAULT NULL;  
+ALTER TABLE `PREFIX_tab_lang` CHANGE `name` `name` VARCHAR(512) DEFAULT NULL;  
 
 ALTER TABLE `PREFIX_attachment` ADD `file_size` bigint(10) unsigned NOT NULL DEFAULT 0 AFTER `file_name`;
 /* PHP:p1603_add_attachment_size(); */;
